55e833425d0a9114f5c15e503f6aa2df3ca47ffa
SereenHanandeh <sereen.hanandeh11@gmail.com>
2025-05-20 11:45:06 +0300

    add

--- clinicApp/src/app/app.component.spec.ts
+++ clinicApp/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'clinicApp' title`, () => {
+  it(`should have the 'clinicSystem' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('clinicApp');
+    expect(app.title).toEqual('clinicSystem');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, clinicApp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, clinicSystem');
   });
 });
