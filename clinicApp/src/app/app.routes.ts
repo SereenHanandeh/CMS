df36d054e06e9b0f9b759bacf1646494677cd305
SereenHanandeh <sereen.hanandeh11@gmail.com>
2025-05-20 12:05:58 +0300

    add routes

--- clinicApp/src/app/app.routes.ts
+++ clinicApp/src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { SignupComponent } from './pages/auth/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component';
+import { authGuard } from './core/guards/Auth/auth.guard';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { DoctorLayoutComponent } from './layouts/doctor-layout/doctor-layout.component';
+import { PatientLayoutComponent } from './layouts/patient-layout/patient-layout.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: SignupComponent },
+
+  {
+    path: 'admin',
+    canActivate: [authGuard],
+    data: { roles: ['admin'] },
+    component: AdminLayoutComponent,
+  },
+  {
+    path: 'doctor',
+    canActivate: [authGuard],
+    data: { roles: ['doctor'] },
+    component: DoctorLayoutComponent,
+  },
+  {
+    path: 'patient',
+    canActivate: [authGuard],
+    data: { roles: ['patient'] },
+    component: PatientLayoutComponent,
+  },
+
+  { path: '**', component: NotFoundComponent },
+];
